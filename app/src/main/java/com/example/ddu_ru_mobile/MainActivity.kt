12320eb975ea66a8face03570ca062fdc701b46e
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2025-08-09 21:22:27 +0900

    #7 feat: 닉네임 정보 입력 UI 구현

--- app/src/main/java/com/example/ddu_ru_mobile/MainActivity.kt
+++ app/src/main/java/com/example/ddu_ru_mobile/MainActivity.kt
@@ -1,15 +1,33 @@
 package com.gildongmu.ddu_ru_mobile
 
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val helloText = findViewById<TextView>(R.id.helloText)
-        helloText.text = "Hello from XML!"
+        val nicknameEdit = findViewById<EditText>(R.id.form_text_input)
+        val btnDone = findViewById<Button>(R.id.btnDone)
+        val mainColor = ContextCompat.getColor(this, R.color.mainColor)
+        val grayColor = ContextCompat.getColor(this, R.color.gray)
+
+        nicknameEdit.addTextChangedListener {
+            val text = it.toString().trim()
+
+
+            if (text.isNotEmpty()) {
+
+                btnDone.setBackgroundColor(mainColor)
+            } else {
+
+                btnDone.setBackgroundColor(grayColor)
+            }
+        }
     }
 }
