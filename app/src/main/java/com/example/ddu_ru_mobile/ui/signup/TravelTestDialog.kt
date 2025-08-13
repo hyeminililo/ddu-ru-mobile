727f9a049b055a5a052c55aeb16845cf439b9f91
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2025-08-13 22:38:55 +0900

    #7 feat: 여행 취향 설문조사 관련 팝업 구현
    현재 디자인적으로 약간의 버그 발생 추후 수정 예정

---
+++ app/src/main/java/com/example/ddu_ru_mobile/ui/signup/TravelTestDialog.kt
@@ -0,0 +1,31 @@
+package com.example.ddu_ru_mobile.ui.signup
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.gildongmu.ddu_ru_mobile.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class TravelTestDialog : BottomSheetDialogFragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.dialog_travel_preference_test_start, container, false
+        )
+        view.findViewById<Button>(R.id.btnLater).setOnClickListener {
+            dismiss()
+        }
+
+        view.findViewById<Button>(R.id.btnStart).setOnClickListener {
+            dismiss()
+            startActivity(Intent(requireContext(), SurveyActivity::class.java))
+
+        }
+        return view
+    }
+}
